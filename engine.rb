1b4ba655fb949ac4545a40db4188973d4ee10c09
congteng <congteng45@gmail.com>
2012-11-13 21:57:58 +0800

    create random order for all characters all enemies must just attaack hero not other enemies

--- engine.rb
+++ engine.rb
@@ -2,16 +2,30 @@ class Engine
   def initialize how_many_enemies = 1
     @hero = create_hero
     @enimies = create_enemies(how_many_enemies)
+    @enemies_count = how_many_enemies
     @counter = -1
+  end
+
+  def start_fight
+    @enimies.each do |enemy|
+      enemy.attack(@hero)
+      @hero.attack(enemy)
+    end
   end
 
   def next
     @counter += 1
-
-    characters = [@hero, @enimies].flatten
+    origin_characters = [@hero, @enimies].flatten
+    characters = origin_characters.sort_by do |character|
+      create_fight_order.index(origin_characters.index(character))
+    end
 
     cursor = @counter % characters.count
     characters[cursor]
+  end
+
+  def create_fight_order
+    (0..@enemies_count).to_a.shuffle
   end
 
   def create_hero
--- spec/engine_spec.rb
+++ spec/engine_spec.rb
@@ -9,12 +9,63 @@ describe Engine do
     end
   end
 
+  describe "#create_fight_order" do
+    it "should return arr with rand right figure " do
+      engine = Engine.new(3)
+      engine.create_fight_order.should be_include(3)
+      engine.create_fight_order.should be_include(1)
+      engine.create_fight_order.should be_include(2)
+      engine.create_fight_order.should be_include(0)
+      engine.create_fight_order.should_not be_include(4)
+    end
+  end
+
+  describe "#start_fight" do
+    before(:each) do
+      @hero = Character.new(1,1)
+
+      Engine.any_instance.stub(:create_hero).and_return(@hero)
+      Engine.any_instance.stub(:create_enemies) do
+        [].tap{ |res| 3.times{ res << Character.new(1,1,"enemy") } }
+      end
+    end
+    context "hero attack enimies" do
+      it "should attack every enemy" do
+        @hero.should_receive(:attack).exactly(3).times
+        Engine.new(3).start_fight
+      end
+    end
+
+    context "enemies attack hero" do
+      it "should attack hero 3 times when 3 enemies" do
+        @hero.should_receive(:under_attack).exactly(3).times
+        Engine.new(3).start_fight
+      end
+    end
+  end
+
   describe "#next" do
+    it "return right order by create_fight_order" do
+      hero = double("hero")
+      e1 = double('e1')
+      e2 = double('e2')
+      e3 = double('e3')
+
+      Engine.any_instance.stub(:create_hero).and_return(hero)
+      Engine.any_instance.stub(:create_enemies).and_return([e1, e2, e3])
+
+      engine = Engine.new(3)
+      engine.stub(:create_fight_order).and_return([1,3,2,0])
+
+      engine.next.should == e1
+      engine.next.should == e3
+      engine.next.should == e2
+      engine.next.should == hero
+    end
     context "1 hero and several enemies" do
       context "hero first" do
         it "should caculate right orders" do
           hero = double('hero')
-
           e1 = double('e1')
           e2 = double('e2')
           e3 = double('e3')
@@ -24,6 +75,7 @@ describe Engine do
           Engine.any_instance.stub(:create_enemies).and_return([e1, e2, e3])
 
           engine = Engine.new(2)
+          engine.stub(:create_fight_order).and_return([0, 1, 2, 3])
 
           engine.next.should == hero
           engine.next.should == e1
@@ -49,6 +101,7 @@ describe Engine do
           end
 
           engine = Engine.new(1)
+          engine.stub(:create_fight_order).and_return([0, 1])
           engine.next.should == hero
           engine.next.should == enemy
           engine.next.should == hero
